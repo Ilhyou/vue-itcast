24517e79e28515d497ce0adaebe703dd35c530db
齐林河 <1277859500@qq.com>
2019-06-20 11:04:24 +0800

    项目初始化-添加了登陆组件，同时配置了相关的路由，下一步实现登陆组件中的结构和样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由模块
+import router from '@/router.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
