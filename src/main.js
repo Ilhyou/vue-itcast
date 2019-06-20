51a939287c991563612c6659898afd9d3930f4ed
齐林河 <1277859500@qq.com>
2019-06-20 12:00:03 +0800

    添加了登录中的表单组件，添加了验证规则，添加了样式，下一步实现登录界面的细节，如添加图标

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 // 引入路由模块
 import router from '@/router.js'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
